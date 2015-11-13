75faebf1b5602b0d18f4598e77db87954877a6f7
Simon <simonboichat44@gmail.com>
2015-11-13 20:11:51 +0000

    add icon when expanded

--- BubbleMenu/BubbleMenu/ViewController.swift
+++ BubbleMenu/BubbleMenu/ViewController.swift
@@ -19,6 +19,8 @@ final class ViewController: UIViewController {
     private var panGesture: UIPanGestureRecognizer?
     
     private var attach: UIAttachmentBehavior?
+    
+    private var labelConstraint : NSLayoutConstraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +30,6 @@ final class ViewController: UIViewController {
         
         // Make it green
         self.greenCircle!.backgroundColor = UIColor.greenColor();
-        
         
         // Place it in the center of our screen
         self.greenCircle!.frame = CGRectMake(CGRectGetMidX(self.view.frame) - 50, CGRectGetMidY(self.view.frame) - 50, 100, 100)
@@ -55,6 +56,7 @@ final class ViewController: UIViewController {
         self.panGesture = UIPanGestureRecognizer(target: self, action: "panning:");
         self.greenCircle!.addGestureRecognizer(self.panGesture!);
         
+        
         let label = UILabel()
         label.text = "item"
         label.intrinsicContentSize()
@@ -62,11 +64,50 @@ final class ViewController: UIViewController {
         self.greenCircle!.addSubview(label)
         
         let xConstraint = NSLayoutConstraint(item: label, attribute: .CenterX, relatedBy: .Equal, toItem: self.greenCircle, attribute: .CenterX, multiplier: 1, constant: 0)
-        let yConstraint = NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: self.greenCircle, attribute: .CenterY, multiplier: 1, constant: 0)
+        self.labelConstraint = NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: self.greenCircle, attribute: .CenterY, multiplier: 1, constant: 0)
         
+        self.greenCircle!.addConstraint(self.labelConstraint!)
         self.greenCircle!.addConstraint(xConstraint)
-        self.greenCircle!.addConstraint(yConstraint)
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: "tap:")
+        //tapGesture.delegate = self
+        self.greenCircle!.addGestureRecognizer(tapGesture)
+    }
+    
+    func tap(tapGesture: UITapGestureRecognizer) {
+        
+        print("item tapped")
+        
+        let item = tapGesture.view
+        item?.removeGestureRecognizer(tapGesture)
+        
+        let label = item?.subviews[0]
+        
+        weak var weakSelf = self
+        
+        UIView.animateWithDuration(0.2, animations: { () -> Void in
+            //label?.removeFromSuperview()
+            
+            item?.transform = CGAffineTransformMakeScale(1.8, 1.8)
+            
+            let image = UIImage(named: "0007")
+            let imageView = UIImageView(image: image)
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            
+            item?.addSubview(imageView)
+            
+            let xConstraint = NSLayoutConstraint(item: imageView, attribute: .CenterX, relatedBy: .Equal, toItem: item, attribute: .CenterX, multiplier: 1, constant: 0)
+            let yConstraint = NSLayoutConstraint(item: imageView, attribute: .CenterY, relatedBy: .Equal, toItem: item, attribute: .CenterY, multiplier: 1, constant: 0)
+            
+            item?.addConstraint(xConstraint)
+            item?.addConstraint(yConstraint)
+            
+            weakSelf?.labelConstraint?.constant = 22
+                
+
+            }) { (result) -> Void in
+               item?.layoutIfNeeded()
+        }
     }
     
     func panning(pan: UIPanGestureRecognizer) {
